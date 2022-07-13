3ec015dc1087cd1a63c72292461e24efe076d062
Den <etlendo@mail.ru>
2022-07-13 20:31:04 +0500

    Урок 6 | Работа с замечаниями к предыдущему ДЗ

--- app/src/main/java/geekbrains/android/myweatherkotlin/utils/Const.kt
+++ app/src/main/java/geekbrains/android/myweatherkotlin/utils/Const.kt
@@ -2,3 +2,4 @@ package geekbrains.android.myweatherkotlin.utils
 
 const val LOCATION_RU = 1
 const val LOCATION_EUROPE = 2
+const val YANDEX_API_KEY = "X-Yandex-API-Key"
--- app/src/main/java/geekbrains/android/myweatherkotlin/utils/WeatherLoader.kt
+++ app/src/main/java/geekbrains/android/myweatherkotlin/utils/WeatherLoader.kt
@@ -7,6 +7,7 @@ import geekbrains.android.myweatherkotlin.BuildConfig
 import geekbrains.android.myweatherkotlin.model.dto.WeatherDTO
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -16,15 +17,20 @@ object WeatherLoader {
     fun request(lat: Double, lon: Double, block: (weather: WeatherDTO) -> Unit) {
 
         val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
-        var myConnection: HttpURLConnection? = null
 
-        myConnection = uri.openConnection() as HttpURLConnection
-        myConnection.readTimeout = 5000
-        myConnection.addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
-        Thread{
-            val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
-            val weatherDTO = Gson().fromJson(getLines(reader), WeatherDTO :: class.java)
-            block(weatherDTO)
+        Thread {
+            var myConnection: HttpURLConnection? = null
+            myConnection = uri.openConnection() as HttpURLConnection
+            try {
+                myConnection.readTimeout = 5000
+                myConnection.addRequestProperty(YANDEX_API_KEY, BuildConfig.WEATHER_API_KEY)
+                val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
+                val weatherDTO = Gson().fromJson(getLines(reader), WeatherDTO :: class.java)
+                block(weatherDTO)
+            } catch (e : Exception) {
+            } finally {
+                myConnection.disconnect()
+            }
         }.start()
     }
 }
