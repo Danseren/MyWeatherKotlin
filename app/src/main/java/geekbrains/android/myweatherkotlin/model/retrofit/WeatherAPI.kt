d75f4d6b2c9aa6a13e204a1c2a66ccbd2f03231a
Den <etlendo@mail.ru>
2022-07-18 20:14:10 +0500

    Урок 7 |  Вспомнил и добавил в константы

--- app/src/main/java/geekbrains/android/myweatherkotlin/model/retrofit/WeatherAPI.kt
+++ app/src/main/java/geekbrains/android/myweatherkotlin/model/retrofit/WeatherAPI.kt
@@ -1,6 +1,7 @@
 package geekbrains.android.myweatherkotlin.model.retrofit
 
 import geekbrains.android.myweatherkotlin.model.dto.WeatherDTO
+import geekbrains.android.myweatherkotlin.utils.WEATHER_API_RETROFIT
 import geekbrains.android.myweatherkotlin.utils.YANDEX_API_KEY
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,7 +10,7 @@ import retrofit2.http.Query
 
 interface WeatherAPI {
 
-    @GET("/v2/informers")
+    @GET(WEATHER_API_RETROFIT)
     fun getWeather(
         @Header(YANDEX_API_KEY) keyValue: String,
         @Query("lat") lat: Double,
